b6054a5be835325bb3c76a9f4d5535eb2623b663
jorge <jorge@examen.local>
2023-06-20 15:34:05 +0000

    vers-2

--- fechajava36.java
+++ fechajava36.java
@@ -2,7 +2,8 @@ public class fechajava36 {
 
 	public static void main(String[] args) throws IOException {
 	
-		System.out.println("Hoy es 14 de Junio de 2023");
+		System.out.println("hoy es 14 de junio de 2023");
+		System.out.println("hoy es miercoles");
 	}
 
 }
